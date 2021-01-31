ec70c2f6090a47c44a82801141eee10a34c5f543
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-01-31 13:11:06 +0530

    valid shuffle

--- src/Strings/ValidShuffle.java
+++ src/Strings/ValidShuffle.java
@@ -1,5 +1,29 @@
 package Strings;
 
-public class ValidShuffle {
+import java.util.HashMap;
 
+public class ValidShuffle {
+    public static void main(String[] args) {
+        String st = "abcde";
+        String st2 = "ctade";
+        System.out.println(isValidShuffle(st, st2));
+    }
+    public static boolean isValidShuffle(String s, String t){
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char item : arr1) {
+            if(map.get(item) != null)
+                map.put(item, map.get(item) + 1);
+            else
+                map.put(item, 1);
+        }
+        for(char item : arr2){
+            if(map.get(item) == null)
+                return false;
+            else if(map.get(item) > 1)
+                return false;
+        }
+        return true;
+    }
 }
