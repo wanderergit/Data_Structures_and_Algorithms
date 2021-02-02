9003eca14a534ed63fab3a2e869191bfbcdda093
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-02-02 15:36:32 +0530

    insertion sort

---
+++ src/Sorting/InsertionSort.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        //assume the first element is sorted
+        for(int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++){
+            int newElement = intArray[firstUnsortedIndex];
+            int i;
+            for(i = firstUnsortedIndex; i > 0 && intArray[i-1] > newElement; i--){
+                intArray[i] = intArray[i-1];
+            }
+            intArray[i] = newElement;
+        }
+        System.out.println(Arrays.toString(intArray));
+
+    }
+}
