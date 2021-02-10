a2a4487a02a0d4dc9d4d673bc783268f5bc7aada
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-02-10 23:17:24 +0530

    knapsack memoized

--- src/DynamicProgramming/problems/Knapsack01.java
+++ src/DynamicProgramming/problems/Knapsack01.java
@@ -1,5 +1,6 @@
 package DynamicProgramming.problems;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Knapsack01 {
@@ -19,8 +20,11 @@ public class Knapsack01 {
         System.out.println("Enter the values in the weight array : ");
         for(int i=0; i<n; i++)
             wt[i] = sc.nextInt();
-
+        int[][] t = new int[n+1][capacity+1];
+        for(int i=0; i<n; i++)
+            Arrays.fill(t[i], -1);
         System.out.println(knapsackRecursive(wt, val, capacity, n));
+        System.out.println(knapsackMemoization(wt, val, capacity, n, t));
 
     }
 
@@ -31,6 +35,18 @@ public class Knapsack01 {
             return Math.max(val[n-1] + knapsackRecursive(wt, val, capacity-wt[n-1], n-1), knapsackRecursive(wt, val, capacity, n-1));
         else if(wt[n-1] > capacity)
             return knapsackRecursive(wt, val, capacity, n-1);
+        return -1;
+    }
+
+    public static int knapsackMemoization(int[] wt, int[] val, int capacity, int n, int[][] t){
+        if(n==0 || capacity==0)
+            return 0;
+        if(t[n][capacity] != -1)
+            return t[n][capacity];
+        if(wt[n-1] <= capacity)
+            return t[n][capacity] = Math.max(val[n-1] + knapsackMemoization(wt, val, capacity-wt[n-1], n-1, t), knapsackMemoization(wt, val, capacity, n-1, t));
+        else if(wt[n-1] > capacity)
+            return t[n][capacity] = knapsackMemoization(wt, val, capacity, n-1, t);
         return -1;
     }
 }
