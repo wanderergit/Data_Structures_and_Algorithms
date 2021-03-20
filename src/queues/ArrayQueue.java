f5c882ddef90680b263c9eaf18a5bacce55da396
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-03-20 10:52:46 +0530

    Queue implementation

--- src/queues/ArrayQueue.java
+++ src/queues/ArrayQueue.java
@@ -1,66 +1,62 @@
 package queues;
 
-import java.util.NoSuchElementException;
 
-public class ArrayQueue {
+public class ArrayQueue{
 
-    private Employee[] queue;
-    private int front;
-    private int back;
+    private int[] q;
+    private int front, back;
+    private int capacity;
 
-    public ArrayQueue(int capacity){
-        queue = new Employee[capacity];
+    public ArrayQueue() {
+        q = new int[10];
+        capacity = 10;
     }
 
-    public void add(Employee employee){
-        if(size() == queue.length-1){
-            //need to resize the backing array
-            Employee[] newArray = new Employee[2 * queue.length];
-            System.arraycopy(queue, 0, newArray, 0, queue.length);
-            queue = newArray;
-        }
-        queue[back] = employee;
-        if(back < queue.length-1){
-            back ++;
-        } else {
-            back = 0;
-        }
+    public ArrayQueue(int cap){
+        q = new int[cap];
+        capacity = cap;
     }
 
-    public Employee remove(){
-        if(isEmpty()){
-            throw new NoSuchElementException();
+    public void enqueue(int ele){
+        if(back == capacity){
+            int[] newArr = new int[2*capacity];
+            System.arraycopy(q, 0, newArr, 0, capacity);
+            q = newArr;
+            newArr = null;
         }
-        Employee employee = queue[front];
-        queue[front] = null;
+        q[back] = ele;
+        back = back + 1;
+    }
+
+    public int dequeue() {
+        int res = q[front];
         front++;
-        if(isEmpty()){
-            front = 0;
-            back = 0;
-        }
-        return employee;
-    }
-
-    public Employee peek(){
-        if(isEmpty()){
-            throw new NoSuchElementException();
-        }
-        return queue[front];
-    }
-
-    public boolean isEmpty(){
-        if(size() == 0)
-            return true;
-        return false;
+        return res;
     }
 
     public int size(){
         return back-front;
     }
 
-    public void printQueue(){
-        for(int i=front; i<back; i++){
-            System.out.println(queue[i]);
-        }
+    public int capacity(){
+        return capacity;
     }
+
+    public int front(){
+        return q[front];
+    }
+
+    public int back(){
+        return q[back];
+    }
+
+    public void printQueue(){
+        System.out.println("Queue : ");
+        System.out.print("Front -> ");
+        for(int i=front; i<back; i++){
+            System.out.print(q[i] + " -> ");
+        }
+        System.out.println("null");
+    }
+
 }
--- src/queues/Main.java
+++ src/queues/Main.java
@@ -2,23 +2,18 @@ package queues;
 
 public class Main {
     public static void main(String[] args) {
-        Employee janeJones = new Employee("jane", "jones", 123);
-        Employee johnDoe = new Employee("john", "doe", 231);
-        Employee marySmith = new Employee("mary", "smith", 332);
-        Employee mikeWilson = new Employee("mike", "wilson", 832);
-        Employee billEnd = new Employee("bill", "end", 938);
 
-        ArrayQueue queue = new ArrayQueue(10);
-        queue.add(janeJones);
-        queue.add(johnDoe);
-        queue.add(marySmith);
-        queue.add(mikeWilson);
-        queue.add(billEnd);
-
-
-        queue.remove();
-        queue.remove();
+        ArrayQueue queue = new ArrayQueue(20);
+        queue.enqueue(68);
+        queue.enqueue(64);
+        queue.enqueue(61);
+        queue.enqueue(66);
+        queue.enqueue(62);
+        queue.enqueue(53);
         queue.printQueue();
-
+        queue.dequeue();
+        queue.dequeue();
+        queue.printQueue();
+        System.out.println("Size : " + queue.size());
     }
 }
