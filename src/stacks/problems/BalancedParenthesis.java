1116a68ec82b1d2c5d685a6162108ca67a66a0b1
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-03-20 11:42:23 +0530

    Check if expression has balanced parenthesis

--- src/stacks/problems/BalancedParenthesis.java
+++ src/stacks/problems/BalancedParenthesis.java
@@ -7,36 +7,31 @@ import java.util.Stack;
 
 public class BalancedParenthesis {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-
-        boolean res = solver(s);
-        if(res)
-            System.out.println("Valid Parenthesis!");
-        else
-            System.out.println("Not a valid parenthesis!");
-
-    }
-
-    public static boolean solver(String s){
-        Stack<Character> st = new Stack<Character>();
-
-        for(int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            if(c == '(')
-                st.push(c);
-            else {
-                if(st.isEmpty())
-                    return false;
-                if(st.peek() == '(')
+        System.out.println("Enter an Expression : ");
+        String exp = br.readLine();
+        char[] chars = exp.toCharArray();
+        Stack<Character> st = new Stack<>();
+        int flag = 0;
+        for(int i=0; i<chars.length; i++){
+            if(chars[i] == '('){
+                st.push(chars[i]);
+            } else if (chars[i] == ')'){
+                if(!st.isEmpty() && st.peek() == '('){
                     st.pop();
-                else
-                    return false;
+                }
+                else{
+                    flag = 1;
+                    break;
+                }
+            } else {
+                continue;
             }
         }
-        if(st.isEmpty())
-            return true;
-        return false;
+        if(flag == 1 || !st.isEmpty()){
+            System.out.println("Not Balanced");
+        } else {
+            System.out.println("Balanced");
+        }
     }
 }
