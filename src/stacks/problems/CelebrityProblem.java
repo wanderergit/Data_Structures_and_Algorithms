a54329d4e440a26e624511e1db9d7ab5d02bc173
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-03-27 09:44:04 +0530

    Celeb problem with comments

--- src/stacks/problems/CelebrityProblem.java
+++ src/stacks/problems/CelebrityProblem.java
@@ -1,40 +1,47 @@
 package stacks.problems;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class CelebrityProblem {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter the size of the array : ");
-        int n = Integer.parseInt(br.readLine());
-        int[][] array = new int[n][n];
-        for(int i=0; i<n; i++)
-            for(int j=0; j<n; j++)
-                array[i][j] = Integer.parseInt(br.readLine());
-        System.out.println(solver(array) + " is the celeb.");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matrix = {{0,0,1,0}, {0,0,1,0},{0,0,0,0},{0,0,1,0}};
+        int length = matrix.length;
+        celebFinder(matrix, length);
     }
-    public static int solver(int[][] array) {
+
+    public static void celebFinder(int[][] matrix, int len){
         Stack<Integer> st = new Stack<>();
-        for(int i=0; i<array[0].length; i++)
+        //pushing the celeb indexes into the stack
+        for(int i=0; i<len; i++){
             st.push(i);
-        while(st.size()!=1){
+        }
+        //taking two indexes at a time and comparing
+        while(st.size() > 1){
             int a = st.pop();
             int b = st.pop();
-            if(array[a][b] == 1 && array[b][a] == 0)
+            if(matrix[a][b] == 1)
                 st.push(b);
             else
                 st.push(a);
         }
-        int res = st.pop();
-        for(int i=0; i<array[0].length; i++){
-            if(array[res][i] != 0)
-                return -1;
-            if(array[i][res] == 0 && i != res)
-                return -1;
+        //when one element is left, check if that element is celeb
+        int ele = st.pop();
+        int flag = 0;
+        for(int i=0; i<len; i++){
+            if(matrix[ele][i] == 1){
+                flag++;
+                break;
+            }
+            if(matrix[i][ele] == 0 && i!=ele){
+                flag++;
+                break;
+            }
         }
-        return res;
+        if(flag == 0)
+            System.out.println(ele + " is the celebrity.");
+        else
+            System.out.println("No one is a celebrity.");
     }
 }
