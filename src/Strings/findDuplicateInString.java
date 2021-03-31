5945915674371dabdcc07e1481f1fa6a2bc90a4a
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-03-31 12:05:32 +0530

    find dups

--- src/Strings/findDuplicateInString.java
+++ src/Strings/findDuplicateInString.java
@@ -1,24 +1,39 @@
 package Strings;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 class FindDuplicateInString
 {
     public static void main(String[] args) {
-        String st = "abcde";
+        String st = "abcssse";
+        System.out.println(isPresentDups(st));
         System.out.println(findDups(st));
     }
-    public static boolean findDups(String s){
+    public static boolean isPresentDups(String s){
+        char[] charArray = s.toCharArray();
+        HashSet<Character> set = new HashSet<>();
+        for(char item : charArray) {
+            if(!set.add(item))
+                return false;
+        }
+        return true;
+    }
+    public static ArrayList<Character> findDups(String s){
         char[] charArray = s.toCharArray();
         HashMap<Character, Integer> map = new HashMap<>();
         for(char item : charArray) {
             if(map.get(item) != null)
-                map.put(item, map.get(item) + 1);
+                map.replace(item, map.get(item)+1);
             else
                 map.put(item, 1);
-            if(map.get(item)>1)
-                return false;
         }
-        return true;
+        ArrayList<Character> res = new ArrayList<>();
+        for(char item : charArray) {
+            if(map.get(item) > 1)
+                res.add(item);
+        }
+        return res;
     }
 }
