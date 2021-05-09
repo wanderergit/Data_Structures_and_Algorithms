e7e1f262480902a8eb535ab64a371d5007ac2020
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-05-09 09:51:55 +0530

    Greedy -> TwoSum

---
+++ src/Greedy/TwoSum.java
@@ -0,0 +1,32 @@
+package Greedy;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 2, 7, 8, 12};
+        System.out.println(twoSum(arr, 20));
+    }
+
+    public static boolean twoSum(int[] arr, int sum){
+        Arrays.sort(arr);
+        int l = 0, r = arr.length - 1;
+        while(l<=r){
+            if(arr[l] + arr[r] == sum) return true;
+            else if(arr[l] + arr[r] < sum) l++;
+            else r--;
+        }
+        return false;
+    }
+
+    public boolean hasTwoSum(int[] arr, int sum){
+        Arrays.sort(arr);
+        int l = 0, r = arr.length - 1;
+        while(l<=r){
+            if(arr[l] + arr[r] == sum) return true;
+            else if(arr[l] + arr[r] < sum) l++;
+            else r--;
+        }
+        return false;
+    }
+}
