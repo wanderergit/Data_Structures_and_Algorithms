a03e4132533bc53b45bc3fddedb772d2b619094e
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-05-15 15:37:46 +0530

    Kadane's algo

---
+++ src/Arrays/Kadane.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class Kadane {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,-2,5};
+        System.out.println("Max sum in a contiguous sub array is : " + maxSumSubarray(arr, arr.length));
+    }
+    public static int maxSumSubarray(int[] arr, int n){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++){
+            currSum += arr[i];
+            if(currSum > maxSum) maxSum = currSum;
+            if(currSum < 0) currSum = 0;
+        }
+        return maxSum;
+    }
+}
